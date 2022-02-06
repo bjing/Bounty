f69d17ec80d3aadea775b8011917ae226658abec
Brian Jing <brjing@seek.com.au>
2022-02-06 15:37:30 +1100

    Clean up CollectionMaker.hs

--- src/CollectionMaker.hs
+++ src/CollectionMaker.hs
@@ -13,11 +13,11 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 --}
-
 {-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE DeriveAnyClass      #-}
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -25,56 +25,35 @@
 {-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeOperators       #-}
-{-# LANGUAGE LambdaCase          #-}
 
 module CollectionMaker where
 
-import           Prelude                (String, show, Show)
-import           Control.Monad          hiding (fmap)
-import           PlutusTx.Maybe
-import qualified Data.Map               as Map
-import           Data.Text              (Text)
-import           Data.Void              (Void)
-import           Plutus.Contract        as Contract
+import           Ledger               hiding (singleton)
+import qualified Ledger.Typed.Scripts as Scripts
+import           Ledger.Value         as Value
 import qualified PlutusTx
-import           PlutusTx.IsData
-import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
-import           Ledger                 hiding (singleton)
-import           Ledger.Ada             as Ada
-import           Ledger.Constraints     as Constraints
-import           Ledger.Index           as Index
-import qualified Ledger.Typed.Scripts   as Scripts
-import qualified Ledger.Contexts                   as Validation
-import           Ledger.Value           as Value
-import           Playground.Contract    (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema, NonEmpty(..) )
-import           Playground.TH          (mkKnownCurrencies, mkSchemaDefinitions, ensureKnownCurrencies)
-import           Playground.Types       (KnownCurrency (..))
-import           Prelude                (Semigroup (..))
-import           Text.Printf            (printf)
-import           GHC.Generics         (Generic)
-import           Data.String          (IsString (..))
-import           Data.Aeson           (ToJSON, FromJSON)
+import           PlutusTx.Prelude     hiding (Semigroup (..), unless)
 
-{-# INLINABLE mkPolicy #-}
+{-# INLINEABLE mkPolicy #-}
 mkPolicy :: AssetClass -> BuiltinData -> ScriptContext -> Bool
 mkPolicy asset _ ctx =
-    mintedAmount < 0 ||
-    traceIfFalse "The CollectionMaker is not present." (nftSum > 0)
+  mintedAmount < 0
+    || traceIfFalse "The CollectionMaker is not present." (nftSum > 0)
   where
     txInfo = scriptContextTxInfo ctx
     txInValues = [txOutValue $ txInInfoResolved txIn | txIn <- txInfoInputs $ scriptContextTxInfo ctx]
-    txOuts = txInfoOutputs txInfo
     nftValues = [assetClassValueOf val asset | val <- txInValues]
     nftSum = sum nftValues
     mintedAmount = case flattenValue (txInfoMint txInfo) of
-          [(cs, collectionTokenName, amt)] | cs == ownCurrencySymbol ctx -> amt
-          _                                                              -> 0
+      [(cs, _, amt)] | cs == ownCurrencySymbol ctx -> amt
+      _                                            -> 0
 
 policy :: AssetClass -> Scripts.MintingPolicy
-policy asset = mkMintingPolicyScript $
-    $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy . mkPolicy ||])
-    `PlutusTx.applyCode`
-    PlutusTx.liftCode asset
+policy asset =
+  mkMintingPolicyScript $
+    $$(PlutusTx.compile [||Scripts.wrapMintingPolicy . mkPolicy||])
+      `PlutusTx.applyCode` PlutusTx.liftCode asset
 
 curSymbol :: AssetClass -> CurrencySymbol
 curSymbol asset = scriptCurrencySymbol $ policy asset
+
